962a46475e8f1007b178ec8a9a14d71755d57555
kortasthekiller <kortas@gmail.com>
2016-02-24 18:38:26 -0600

    adding comments

--- src/com/josh/Main.java
+++ src/com/josh/Main.java
@@ -7,23 +7,23 @@ public class Main {
     public static void main(String[] args) {
         //ask how many training times to enter
         Scanner amountScanner = new Scanner(System.in);
-        System.out.println("How many training runs do you want to enter?");
+        System.out.println("How many training runs do you want to enter?");   //ask how many training runs they want to enter
         int getcounter = amountScanner.nextInt();
 
         //getInfo(getcounter);
-        HashMap returnLakeInfo = getInfo(getcounter);
-        for (Object ob: returnLakeInfo.keySet()) {
-            System.out.print(ob);
-            System.out.print(returnLakeInfo.get(ob));
+        HashMap returnLakeInfo = getInfo(getcounter);  //call function getInfo
+
+        System.out.println("finished");    //testing to let me know when code is finished
+
         }
-        System.out.println("finished");
-
-    }
 
 
-    static HashMap getInfo(int counter) {
 
-        HashMap<String, Double> lakeInfo = new HashMap();
+
+
+    static HashMap getInfo(int counter) {    //getInfo function
+
+        HashMap<String, Double> lakeInfo = new HashMap();    //create HashMap to store lake name and run time
         for (int x = 0; x< counter; x++) {
             Scanner lakeScanner = new Scanner(System.in);
         System.out.println("Enter the lake name:");
